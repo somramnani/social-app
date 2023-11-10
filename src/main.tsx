8f7e896ef26cdde4c2536928559a9fbb3f0e01f4
somramnani <som.ramnani@gmail.com>
2023-11-10 16:15:01 -0500

    add postscss.config.js, edit global.css, move globals.css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
+import "./globals.css";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<App />);

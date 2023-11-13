75f7f1c4d5d741ae658c049a6fb5a011d0b45eb1
somramnani <som.ramnani@gmail.com>
2023-11-13 14:42:09 -0500

    add button from shadcn

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,19 @@
+import { Outlet, Navigate } from "react-router-dom";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <section>
+          <Outlet />
+        </section>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;

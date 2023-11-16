148d604b298320b2c2947c70eb0dc9849b5db34d
somramnani <som.ramnani@gmail.com>
2023-11-16 17:48:26 -0500

    edit Form & add styles

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -22,7 +22,10 @@ const SignUpForm = () => {
   const form = useForm<z.infer<typeof SignUpValidation>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
     },
   });
 
@@ -35,25 +38,44 @@ const SignUpForm = () => {
   return (
     <div>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
+        <div className="sm:w-420 flex-center flex-col">
+          <img src="/assets/images/logo.svg"></img>
+
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+            Create a new account
+          </h2>
+          <p className="text-light-3 small-medium md:base-regular mt-2">
+            To use Snapgram enter your account details
+          </p>
+
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex-col gap-5 w-full mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      className="shad-input"
+                      placeholder="shadcn"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </div>
       </Form>
     </div>
   );
